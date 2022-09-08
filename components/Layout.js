33058ac5fc77784540f2216d296adc8966e25ad9
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-09-08 19:45:55 +0800

    add layout

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Nav from './Nav';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <link
+          rel='icon'
+          type='image/svg+xml'
+          href='/assets/favicon-32x32.png'
+        />
+      </Head>
+      <div className='mx-auto grid w-full max-w-[1920px] place-items-center'>
+        <Nav />
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/Nav.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Nav = () => {
+  return (
+    <nav className='flex w-full flex-col items-center border-b border-solid border-mercury py-6'>
+      <div className='flex w-[87.2%] items-center justify-between'>
+        <Link href='/'>
+          <a className='relative h-8 w-[7.065rem]'>
+            <Image
+              layout='fill'
+              objectFit='cover'
+              src='/assets/shared/logo.svg'
+              alt='logo'
+            />
+          </a>
+        </Link>
+        <Link href=''>
+          <a className='font-link-1 text-[0.563rem] leading-[0.688rem] tracking-[1.93px] text-boulder'>
+            Start slideshow
+          </a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
